248077cbe2aa017135cb48d9907ba692f4aff6a2
Yoav C <33702102+Yoavcwix@users.noreply.github.com>
2024-03-28 22:47:49 +0200

    add onhover to text5

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -3,5 +3,9 @@ $w.onReady(function () {
   $w("#txtHeading").onClick((event) => {
     $w("#txtHeading").text = "Hi there dude. Im from another branch";
     $w("#text5").text = "look, I am also a different text from another branch";
+
+    $w("#text5").onMouseIn(() => {
+      $w("#text5").text = "oh look, I changed!";
+    });
   }); // txtHeading OnClick function Ends
 }); //Onready Closed
