f4ae6415adb94554bc94726e426a8790f7ac64db
Yoav C <33702102+Yoavcwix@users.noreply.github.com>
2024-03-28 16:51:12 +0200

    Update Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -2,6 +2,7 @@
 $w.onReady(function () {
   $w("#txtHeading").onClick((event) => {
     $w("#txtHeading").text = "Hi there dude. Im from another branch";
-    $w("#text5").text = "look, I am also a different text from another branch";
+    // $w("#text5").text = "look, I am also a different text from another branch";
+    //lets not make any more changes
   }); // txtHeading OnClick function Ends
 }); //Onready Closed
