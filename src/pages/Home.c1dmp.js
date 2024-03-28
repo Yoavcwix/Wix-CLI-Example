175526f9684fd509afa8b6b332c1524114ba1c78
Yoav C <33702102+Yoavcwix@users.noreply.github.com>
2024-03-28 16:52:46 +0200

    Revert change from Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -3,6 +3,5 @@ $w.onReady(function () {
   $w("#txtHeading").onClick((event) => {
     $w("#txtHeading").text = "Hi there dude. Im from another branch";
     // $w("#text5").text = "look, I am also a different text from another branch";
-    //lets not make any more changes
   }); // txtHeading OnClick function Ends
 }); //Onready Closed
