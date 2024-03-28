093b913f0d14b377cc12819ae487ca90fbad1c84
Yoavcwix <yoavc@wix.com>
2024-03-28 14:17:12 +0200

    add word 'from'

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,6 +1,6 @@
 //OnReady function - runs when pages complete loading
 $w.onReady(function () {
   $w("#txtHeading").onClick((event) => {
-    $w("#txtHeading").text = "Hi there dude. Im another branch";
+    $w("#txtHeading").text = "Hi there dude. Im from another branch";
   }); // txtHeading OnClick function Ends
 }); //Onready Closed
