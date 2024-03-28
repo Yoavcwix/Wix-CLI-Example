5b7a6ce70627c9b5f5f66a5267ed63f371869e56
Yoavcwix <yoavc@wix.com>
2024-03-28 14:04:39 +0200

    edit heading text onClick

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,10 +1,6 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
-
+//OnReady function - runs when pages complete loading
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
-});
+  $w("#txtHeading").onClick((event) => {
+    $w("#txtHeading").text = "Hi there";
+  }); // txtHeading OnClick function Ends
+}); //Onready Closed
