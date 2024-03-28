d6197274b36f82eaa35d55c5b0b4a16e707efa63
yoavatwix <165282304+yoavatwix@users.noreply.github.com>
2024-03-28 14:42:40 +0000

    add onHover

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -4,4 +4,9 @@ $w.onReady(function () {
     $w("#txtHeading").text = "Hi there dude. Im from another branch";
     $w("#text5").text = "look, I am also a different text from another branch";
   }); // txtHeading OnClick function Ends
+
+  $w("#txtHeading").onHover(() => {
+    "Click to change the text to something cool!"
+  })
+
 }); //Onready Closed
